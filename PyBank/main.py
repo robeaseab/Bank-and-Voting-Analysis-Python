694ae8fcac2d2f5df45ca19bbbfbd82c1f06c37b
searobea <searobea@gmail.com>
2019-03-03 18:43:15 -0600

    final edits made

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,10 +3,11 @@ import csv
 #print(os.getcwd)
 csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 
-months= []
-#total= int
+months=[]
+total= int
 total= 0 
-profit_losses= []
+profit_losses=[]
+change=[]
 
 with open(csvpath) as csvfile:
     #read header
@@ -14,41 +15,56 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     #print(f"CSV Header: {csv_header}")
     
+    #put first entries in month and profit_losses lists
+    csv_firstrow = next(csvreader)
+    months.append(str(csv_firstrow[0]))
+    profit_losses.append(int(csv_firstrow[1]))
+
+    
+    
     for row in csvreader:
-        #count months
+        
+        #add to list of months
         months.append(row[0])
 
-        #create array of profit/loss values
+        #add to list of daily change values
+        change.append((int(row[1])) - profit_losses[-1])
+
+        #add to list of profit/loss values
         profit_losses.append((int(row[1])))
 
-        greatest_increase = max(profit_losses)
-        greatest_decrease = min(profit_losses)
+        #define greatest increase and decrease
+        greatest_increase = max(change)
+        greatest_decrease = min(change)
 
-        if int(row[1]) == greatest_increase:
+        #determine greatest increase and decrease as well as corresponding month in which it occured
+        if change[-1] == greatest_increase:
             greatest_increase_month = str(row[0])
 
-        if int(row[1]) == greatest_decrease:
+        if change[-1] == greatest_decrease:
             greatest_decrease_month = str(row[0])
-    
-    number_of_months = len(months)
-    total = sum(profit_losses) 
-
-    
-    change = [profit_losses[i] - profit_losses[i-1] for i in range(1 , len(profit_losses))]
-    print(change)
+        
+    #CHECKING WORK
+    """    
+    print(f"CHANGE:                         {change}")
+    print(f"MONTHS:                         {months}")
+    print(f"Profit Losses:                  {profit_losses}")
+    print(greatest_increase_month)
+    print(greatest_decrease_month)
+    """
+    #calculate average change and total
     avg_change = sum(change) / len(change)
+    total = sum(profit_losses)
 
     #Header
     print("Financial Analysis")
     print("---------------------------------------")
     #Total Months
-    print("Total Months: " + str(number_of_months))
+    print("Total Months: " + str(len(months)))
     #Total
     print("Total: $" + str(total))
-    
-    
     #Average Change
-    print("Average  Change: $" + str(avg_change))
+    print(f"Average  Change: $ {str(round(avg_change,2))}")
     #Greatest Increase
     print(f"Greatest Increase in Profits: {str(greatest_increase_month)} ( {str(greatest_increase)} )")
     #Greatest Decrease
@@ -61,8 +77,7 @@ with open("PyBank_results.txt", "w") as PyBank_results:
     ---------------------------------------
     Total Months: {str(len(months))}
     Total: $ {str(total)}
-    Average  Change: $ {str(avg_change)}
+    Average  Change: $ {str(round(avg_change,2))}
     Greatest Increase in Profits: {str(greatest_increase_month)} ( {str(greatest_increase)} )
     Greatest Decrease in Profits: {str(greatest_decrease_month)} ( {str(greatest_decrease)} )
     ---------------------------------------""")
-    
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -15,7 +15,9 @@ with open(csvpath) as csvfile:
     #get list of votes by last name
     for row in csvreader:
         votes.append(row[2])
+
 num_votes = len(votes)
+
 #get list of candidates        
 for candidate in votes:
     if candidate not in candidateList:
@@ -26,6 +28,7 @@ CorreyVotes = 0
 LiVotes = 0
 OTooleyVotes = 0
 
+#Get numner of votes for each candidate
 for candidate in votes:
     if candidate == "Khan":
        KhanVotes = KhanVotes + 1
@@ -35,12 +38,14 @@ for candidate in votes:
        LiVotes = LiVotes + 1
     if candidate == "O'Tooley":
        OTooleyVotes = OTooleyVotes + 1
-  
+
+#Calculate percent of votes for each candidate
 Khan_percent = (KhanVotes / num_votes) * 100
 Correy_percent = (CorreyVotes / num_votes) * 100
 Li_percent = (LiVotes / num_votes) * 100
 OTooley_percent = (OTooleyVotes / num_votes) * 100
 
+#determine winner
 if KhanVotes > CorreyVotes and KhanVotes > LiVotes and KhanVotes > OTooleyVotes:
     winner = "Khan"
 elif CorreyVotes > KhanVotes and CorreyVotes > LiVotes and CorreyVotes > OTooleyVotes:
@@ -50,30 +55,34 @@ elif LiVotes > KhanVotes and LiVotes  > CorreyVotes and LiVotes  > OTooleyVotes:
 elif OTooleyVotes  > KhanVotes and OTooleyVotes  > CorreyVotes and OTooleyVotes  > LiVotes:
     winner = "O'Tooley"
 
+#Print results in terminal
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes:  {num_votes} ")
 print("-------------------------")
-print(f"Khan: {Khan_percent}% ({KhanVotes})")
-print(f"Correy: {Correy_percent}% ({CorreyVotes})")
-print(f"Li: {Li_percent}% ({LiVotes})")
-print(f"O'Tooley: {OTooley_percent}% ({OTooleyVotes})")
+print(f"Khan: {round(Khan_percent, 3)}% ({KhanVotes})")
+print(f"Correy: {round(Correy_percent, 3)}% ({CorreyVotes})")
+print(f"Li: {round(Li_percent, 3)}% ({LiVotes})")
+print(f"O'Tooley: {round(OTooley_percent, 3)}% ({OTooleyVotes})")
 print("-------------------------")
 print(f"Winner: {winner}")
 print("-------------------------")
 
 #PyPoll_results = open(Python-Challenge/PyPoll/main.py/PyPoll_results.txt)
 #with open((os.path.join("~", "Python-Challenge", "PyPoll", "PyPoll_results.txt")), "w") as PyPoll_results:
+
+
+#Print results to .txt
 with open("PyPoll_results.txt", "w") as PyPoll_results:
     PyPoll_results.write(f"""
     Election Results
     -------------------------
-    Total Votes:  {num_votes}
+    Total Votes:  {num_votes} 
     -------------------------
-    Khan: {Khan_percent}% ({KhanVotes})
-    Correy: {Correy_percent}% ({CorreyVotes})
-    Li: {Li_percent}% ({LiVotes})
-    O'Tooley: {OTooley_percent}% ({OTooleyVotes})
+    Khan: {round(Khan_percent, 3)}% ({KhanVotes})
+    Correy: {round(Correy_percent, 3)}% ({CorreyVotes})
+    Li: {round(Li_percent, 3)}% ({LiVotes})
+    O'Tooley: {round(OTooley_percent, 3)}% ({OTooleyVotes})
     -------------------------
     Winner: {winner}
     -------------------------""")
